6eae21533f4c5cdfcf5095914f0b833a54de8f37
Matthew Lorber <matthew.lorber@gmail.com>
2023-02-10 12:34:17 -0600

    js in html update

---
+++ js/external.js
@@ -0,0 +1,26 @@
+alert("Welcome to my website!")
+
+let userInput = prompt("What is your favorite Color?");
+
+alert("Great, " + userInput + " is my favorite color too!");
+
+let tlm = prompt("How many days did you keep The Little Mermaid?");
+let bb = prompt("How many days did you keep Brother Bear?");
+let herc = prompt("How many days did you keep Hercules?");
+let paymentdue = ((tlm *3) + (bb *3) + (herc * 3));
+alert("Please pay Blockbuster " + paymentdue + " dollars.");
+
+let Google = 400 * prompt("How many hours did you work for Google this week?")
+let Amazon = 380 * prompt( "How many hours did you work for Amazon this week?")
+let Facebook = 350 * prompt("How many hours did you work for Facebook this week?")
+
+let paycheck = Google + Amazon + Facebook
+
+alert("You earned $" + paycheck + " this week!")
+
+
+
+
+
+
+
---
+++ js/javascript-with-html.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//let didConfirm = confirm("Would you like to confirm?")
+//console.log(didConfirm)
+
+let userName = propmpt("What is your name?");
+
+console.log(`The user's name is ${userName}`);
