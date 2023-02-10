15213ab5ba37886a03265abddabf9f662578216a
Matthew Lorber <matthew.lorber@gmail.com>
2023-02-10 14:06:29 -0600

    updates to javascript in html

--- js/external.js
+++ js/external.js
@@ -10,13 +10,29 @@ let herc = prompt("How many days did you keep Hercules?");
 let paymentdue = ((tlm *3) + (bb *3) + (herc * 3));
 alert("Please pay Blockbuster " + paymentdue + " dollars.");
 
-let Google = 400 * prompt("How many hours did you work for Google this week?")
-let Amazon = 380 * prompt( "How many hours did you work for Amazon this week?")
-let Facebook = 350 * prompt("How many hours did you work for Facebook this week?")
+let Google = 400 * prompt("How many hours did you work for Google this week?");
+let Amazon = 380 * prompt( "How many hours did you work for Amazon this week?");
+let Facebook = 350 * prompt("How many hours did you work for Facebook this week?");
 
-let paycheck = Google + Amazon + Facebook
+let paycheck = Google + Amazon + Facebook;
 
-alert("You earned $" + paycheck + " this week!")
+alert("You earned $" + paycheck + " this week!");
+
+
+let scheduleconflicts = false
+let maxClassSize = prompt ("What is the maximum class size for this class?");
+let currentClassSize = prompt ("What is the current class size?");
+let canEnroll = !scheduleconflicts && (currentClassSize < maxClassSize);
+alert(canEnroll)
+
+
+let minCartSize = 2;
+let cartSize = prompt ("How many items are in your cart?");
+let productexpiration = confirm(" Is your offer expired?");
+let premiumMember = confirm( "Are you a premium member?");
+
+let canOfferApply = (premiumMember || (cartSize >= minCartSize) && !productexpiration)
+alert(canOfferApply)
 
 
 
