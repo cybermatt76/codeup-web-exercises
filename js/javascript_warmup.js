da1218a61e1ab4ab448179e24db9014725a21d27
Matthew Lorber <matthew.lorber@gmail.com>
2023-03-06 12:05:32 -0600

    html forms exercise

--- js/javascript_warmup.js
+++ js/javascript_warmup.js
@@ -52,18 +52,18 @@
  * All test arrays will have at least one element and are valid.
  */
 
-function minMax(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-        return undefined; // return undefined for invalid input
-    }
-
-    const min = Math.min(...numbers);
-    const max = Math.max(...numbers);
-
-    return [min, max];
-}
-
-console.log(minMax([8, 4, 2, 9, 198, 1, 5678]))
+// function minMax(numbers) {
+//     if (!Array.isArray(numbers) || numbers.length === 0) {
+//         return undefined; // return undefined for invalid input
+//     }
+//
+//     const min = Math.min(...numbers);
+//     const max = Math.max(...numbers);
+//
+//     return [min, max];
+// }
+//
+// console.log(minMax([8, 4, 2, 9, 198, 1, 5678]))
 
 
  // * Drink Sorting
@@ -76,23 +76,23 @@ console.log(minMax([8, 4, 2, 9, 198, 1, 5678]))
  // * Examples
  // * >> sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
 
-drinks = [
-    {name: "lemonade", price: 50},
-    {name: "lime", price: 10}
-]
-
-function sortDrinkByPrice(drinks) {
-    if (!Array.isArray(drinks) || drinks.length === 0) {
-        return []; // return an empty array for invalid input
-    }
-
-    // sort the drinks array in ascending order by price
-    drinks.sort((a, b) => a.price - b.price);
-
-    return drinks;
-}
-
-console.log(sortDrinkByPrice(drinks))
+// drinks = [
+//     {name: "lemonade", price: 50},
+//     {name: "lime", price: 10}
+// ]
+//
+// function sortDrinkByPrice(drinks) {
+//     if (!Array.isArray(drinks) || drinks.length === 0) {
+//         return []; // return an empty array for invalid input
+//     }
+//
+//     // sort the drinks array in ascending order by price
+//     drinks.sort((a, b) => a.price - b.price);
+//
+//     return drinks;
+// }
+//
+// console.log(sortDrinkByPrice(drinks))
 
 
 
@@ -114,3 +114,135 @@ console.log(sortDrinkByPrice(drinks))
  *        { name: "Martin",  age: 16, budget: 1600 }
  *    ]) ➞ 62600
  */
+
+const budget1 = [
+    { name: "John", age: 21, budget: 23000 },
+    { name: "Steve",  age: 32, budget: 40000 },
+    { name: "Martin",  age: 16, budget: 2700 }
+]
+// function getBudgets(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i].budget;
+//     }
+//     return sum;
+// }
+
+
+
+function getBudgets(arr) {
+    return arr.reduce((acc, cur) => acc + cur.budget, 0);
+}
+
+console.log(getBudgets(budget1))
+
+const cities = [
+    {
+        name: "New York City",
+        state: "New York",
+        population: 8336817
+    },
+    {
+        name: "Los Angeles",
+        state: "California",
+        population: 3979576
+    },
+    {
+        name: "Chicago",
+        state: "Illinois",
+        population: 2693976
+    },
+    {
+        name: "Houston",
+        state: "Texas",
+        population: 2320268
+    },
+    {
+        name: "Phoenix",
+        state: "Arizona",
+        population: 1680992
+    },
+    {
+        name: "Philadelphia",
+        state: "Pennsylvania",
+        population: 1584064
+    },
+    {
+        name: "San Antonio",
+        state: "Texas",
+        population: 1547253
+    },
+    {
+        name: "San Diego",
+        state: "California",
+        population: 1423851
+    },
+    {
+        name: "Dallas",
+        state: "Texas",
+        population: 1343573
+    },
+    {
+        name: "San Jose",
+        state: "California",
+        population: 1030119
+    },
+    {
+        name: "Austin",
+        state: "Texas",
+        population: 964254
+    },
+    {
+        name: "Jacksonville",
+        state: "Florida",
+        population: 903889
+    },
+    {
+        name: "Fort Worth",
+        state: "Texas",
+        population: 895008
+    },
+    {
+        name: "Columbus",
+        state: "Ohio",
+        population: 892533
+    },
+    {
+        name: "San Francisco",
+        state: "California",
+        population: 883305
+    },
+    {
+        name: "Charlotte",
+        state: "North Carolina",
+        population: 885708
+    },
+    {
+        name: "Indianapolis",
+        state: "Indiana",
+        population: 876384
+    },
+    {
+        name: "Seattle",
+        state: "Washington",
+        population: 753675
+    },
+    {
+        name: "Denver",
+        state: "Colorado",
+        population: 727211
+    },
+    {
+        name: "Washington",
+        state: "District of Columbia",
+        population: 702455
+    }
+];
+
+for (let i = 0; i < cities.length; i++) {
+    console.log(`${cities[i].name}, ${cities[i].state}`);
+}
+
+function isInTexas(cityObject){
+    return cityObject.state === "Texas";
+    }
